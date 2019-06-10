91cb583c24f4a5920dd0468259c91d04d4fc2abd
Shivani <shivanimayecha0908@gmail.com>
2019-06-11 01:49:54 +0530

    Add project files.

---
+++ EntityFrameworkPra2/EntityFrmwrkPra2.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EntityFrmwrkPra2.aspx.cs" Inherits="EntityFrameworkPra2.EntityFrmwrkPra2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView runat="server" ID="gvProducts" AutoGenerateColumns="false">
+    <Columns>
+        <asp:BoundField DataField="id" HeaderText="Product ID" />
+        <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+        <asp:BoundField DataField="Price" HeaderText="Price" DataFormatString="{0:N2}" />
+        <asp:BoundField DataField="CategoryName" HeaderText="Category Name" />
+        <asp:BoundField DataField="Description" HeaderText="Description" />
+    </Columns>
+</asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EntityFrameworkPra2/EntityFrmwrkPra2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntityFrameworkPra2
+{
+    public partial class EntityFrmwrkPra2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                using (demoEntities entities = new demoEntities())
+                {
+                    product pro = new product();
+                    category cat = new category();
+                    var details = (from p in entities.products
+                                   join c in entities.categories on p.CategoryID equals c.CategoryID
+                                   orderby p.id
+                                   select new
+                                   {
+                                       id = p.id,
+                                       ProductName = p.ProductName,
+                                       price = p.Price,
+                                       categoryName = c.CategoryName,
+                                       Description = c.Description
+                                   }).Take(10).ToList();
+                    gvProducts.DataSource = details;
+                    gvProducts.DataBind();
+                }
+            }
+        }
+    }
+}
